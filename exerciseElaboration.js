2b6fae174ce83f2d2f05c72e69fe27cd9824638f
alex8616 <49254549+alex8616@users.noreply.github.com>
2019-04-05 14:39:42 -0400

    Add files via upload

--- exerciseElaboration.js
+++ exerciseElaboration.js
@@ -1,9 +1,18 @@
 function exerciseElaboration(p,n){
-    var cad = p;
-    for (var i = 0; i < n; i++) {
-          cad = cad + '0';
-          cad = cad + p;
-          return cad;
+    n = n*n;
+    var aux = 0;
+    if (n >= 1){
+      while (n > 0) {
+        aux += n % 10;
+        n = parseInt (n/10);
+      }
+      return p*100+p;
+    } else {
+      while (n > 0) {
+        aux += aux % 10;
+        n = parseInt (n/10);
+      }
+      return p*10+p;
     }
-}
-console.log(exerciseElaboration(1,2));
+  }
+console.log(exerciseElaboration(5,1)*exerciseElaboration(5,1));
