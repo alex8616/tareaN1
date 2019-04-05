01be78d7b7f48d05f2dece826c71fb9e3824427a
alex8616 <49254549+alex8616@users.noreply.github.com>
2019-04-05 14:13:52 -0400

    Add files via upload

--- matrixElementsSum.js
+++ matrixElementsSum.js
@@ -1,19 +1,20 @@
-<script>
-//var matriz=[[1, 1, 1, 0], [0, 5, 0, 1], [2, 1, 3, 10]];
-var matriz=[[0, 1, 1, 2], [0, 5, 0, 0],[2, 0, 3, 3]];
-var aux = 0;
-for (var i = 0; i < matriz.length; i++) {
-  for (var j = 0; j < matriz[i].length; j++) {
-      if(matriz[i][j]!=0){
-        if(i==0){
-          aux=aux+matriz[i][j];
-        } else {
-          if(matriz[i-1][j-1]!=0){
+function mat(){
+  //var matriz=[[1, 1, 1, 0], [0, 5, 0, 1], [2, 1, 3, 10]];
+  var matriz=[[0, 1, 1, 2], [0, 5, 0, 0],[2, 0, 3, 3]];
+  var aux = 0;
+  for (var i = 0; i < matriz.length; i++) {
+    for (var j = 0; j < matriz[i].length; j++) {
+        if(matriz[i][j]>=1){
+          if(i==0){
             aux=aux+matriz[i][j];
+          } else {
+            if(matriz[i-1][j-1]!=0){
+              aux=aux+matriz[i][j];
+            }
           }
         }
-      }
+    }
   }
+  return aux;
 }
-alert(aux);
-</script>
+console.log(mat());
